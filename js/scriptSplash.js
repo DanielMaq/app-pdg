16648dc626ccd448ea02aa1b27aa79ea818044cd
Lucho <lucho.bonatti@devteam.com>
2015-03-04 16:15:43 -0300

    1

--- js/scriptSplash.js
+++ js/scriptSplash.js
@@ -2,7 +2,6 @@ $('#splashPage').live( 'pageinit',function(event){
 
     setTimeout(function(){
         var logged = $.isEmptyObject(localStorage.getItem("userID"));
-        alert(logged)
         if( logged ){
             window.location.href = 'login.html';
         }else{
