d7dc26ce2b217ae4a7152e9b8151559add56b2a8
Lucas Burella <lucas.burella@4rsoluciones.com>
2015-06-05 13:49:53 -0300

    Menu

--- js/jssite.js
+++ js/jssite.js
@@ -454,7 +454,7 @@ $('a.home').on('touchstart', function () {
 $(document).ready(function () {
     $('body').fadeIn('slow');
 
-    if ($('#campaignPage').length != 0) return
+    if ($('#campaignPage').length != 0) return;
     // Bind the swiperightHandler callback function to the swipe event on div.box
     var ts;
     $(document).bind('touchstart', function (e){
@@ -466,10 +466,14 @@ $(document).ready(function () {
        var te = e.originalEvent.changedTouches[0].clientY;
        if(ts > te+5){
           $('#pageHeader').fadeOut();
+          $('#pageHeader #menu').hide();
        }else if(ts < te-5){
           setTimeout(function () {
               $('#pageHeader').fadeIn();
-          }, 250);
+              setTimeout(function () {
+                    $('#pageHeader #menu').show();
+                }, 500);
+          }, 1000);
        }
     });
 });
