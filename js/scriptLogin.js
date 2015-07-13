2091307dab17390fcf29be9da2661956798eae74
Lucho <lucho.bonatti@devteam.com>
2015-07-13 15:25:07 -0300

    23

--- js/scriptLogin.js
+++ js/scriptLogin.js
@@ -1,6 +1,6 @@
-$('#loginPage').live( 'pageinit',function(event){
+$('#loginPage').live('pageinit', function (event) {
 
-    if(localStorage.getItem('lastlogin') != null ){
+    if (localStorage.getItem('lastlogin') != null) {
         var $autoLogin = $.parseJSON(localStorage.getItem('lastlogin'))
         $('input.username').val($autoLogin.user)
         $('input.password').val($autoLogin.pw)
@@ -10,14 +10,14 @@ $('#loginPage').live( 'pageinit',function(event){
 
     setHeights();
 
-    $('#loginPage #loginForm').on('submit', function(e){
+    $('#loginPage #loginForm').on('submit', function (e) {
         $('#loginPage .btn.ingreso').trigger('click');
         e.preventDefault();
         return false;
     });
 
 
-    $('#loginPage .btn.ingreso').on('click', function(e){ //LOGIN
+    $('#loginPage .btn.ingreso').on('click', function (e) { //LOGIN
 
         e.preventDefault();
         showError('vaciar');
@@ -27,69 +27,69 @@ $('#loginPage').live( 'pageinit',function(event){
 
         var sendOk = 0;
 
-        if($.trim(username).length === 0 ){
+        if ($.trim(username).length === 0) {
             showError('El email es requerido.');
             $('input.username').focus();
             return;
-        }else{
+        } else {
             sendOk = sendOk + 1;
         }
 
-        if($.trim(pass).length === 0){
+        if ($.trim(pass).length === 0) {
             showError('La contraseña no puede ser vacía.');
             $('input.password').focus();
             return;
-        }else{
+        } else {
             sendOk = sendOk + 1;
         }
 
-        if(!validateEmail(username)){
+        if (!validateEmail(username)) {
             showError('El Email ingresado no es válido');
             $('input.username').focus();
             return;
-        }else{
+        } else {
             sendOk = sendOk + 1;
         }
-        if(sendOk == 3){
+        if (sendOk == 3) {
             //TODO: borrar datos fijos
 
             /*
-                username = 'gpetti@aterrizar.com.ar';
-                pass = '30577262124';
-            */
+             username = 'gpetti@aterrizar.com.ar';
+             pass = '30577262124';
+             */
             $.ajax({
-                url: webServicesUrl+"login.php",
-                type:'POST',
+                url: webServicesUrl + "login.php",
+                type: 'POST',
                 timeout: 5000,
-                data:{email: username, password: pass},
-                beforeSend: function(){
+                data: {email: username, password: pass},
+                beforeSend: function () {
                     $('.innerContainer *').not('h2').hide()
                     $('.innerContainer p.loading').remove();
                     $('.innerContainer').append('<p class="loading" style="margin-top:20px">Ingresando...</p>')
                 },
-                success:function(result){
+                success: function (result) {
                     var r = $.parseJSON(result);
-                    if (r.data && r.data.status && r.data.status == 'success'){
-                        var $lastlogin = '{"user": "'+$('input.username').val()+'", "pw": "'+$('input.password').val()+'"}';
+                    if (r.data && r.data.status && r.data.status == 'success') {
+                        var $lastlogin = '{"user": "' + $('input.username').val() + '", "pw": "' + $('input.password').val() + '"}';
                         localStorage.setItem('lastlogin', $lastlogin);
                         //showError('Ingresando...', 1);
                         localStorage.setItem("userID", r.data[0].userID);
-                        window.location.href="home.html";
-                    }else{
+                        window.location.href = "home.html";
+                    } else {
                         showError(r.data.message);
                         $('.innerContainer *').show()
                         $('.innerContainer p.loading').hide();
                         $('input.username').focus();
                     }
                 },
-                error:function(error){
+                error: function (error) {
                     showError('Error de conexión.');
                     $('.innerContainer *').show()
                     $('.innerContainer p.loading').hide();
                     $('input.username').focus();
                 }
             });
-    }
+        }
         return;
     });
 });
