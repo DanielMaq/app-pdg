3c0ad196cf0a6795dd5aae87f456c7a2823a92fb
Lucas Burella <lucas.burella@4rsoluciones.com>
2015-08-06 16:33:34 -0300

    rr

--- js/scriptDevice.js
+++ js/scriptDevice.js
@@ -1,159 +1,166 @@
-var badgeCount = 0;
+$('#loginPage').live('pageinit', function (event) {
+
+    // Si estamos en la página de login, intentamos quitar el registro de usuario.
+    unRegisterDevice();
+
+    // Colocamos los datos del último login en el formulario
+    if (localStorage.getItem('lastlogin') != null) {
+        var $autoLogin = $.parseJSON(localStorage.getItem('lastlogin'))
+        $('input.username').val($autoLogin.user)
+        $('input.password').val($autoLogin.pw)
+    }
+
+    // Verificamos que no esté logueado.
+    is_logged();
+
+    // Ajustamos pantalla
+    setHeights();
+
+    // Configuramos el envío del formulario
+    $('#loginPage #loginForm').on('submit', function (e) {
+        $('#loginPage .btn.ingreso').trigger('click');
+        e.preventDefault();
+        return false;
+    });
+
+
+    // Enviamos el formulario de login
+    $('#loginPage .btn.ingreso').on('click', function (e) {
+
+        e.preventDefault();
+        showError('vaciar');
+
+        var username = $('.campo.username').val();
+        var pass = $('.campo.password').val();
+
+        var sendOk = 0;
+
+        if ($.trim(username).length === 0) {
+            showError('El email es requerido.');
+            $('input.username').focus();
+            return;
+        } else {
+            sendOk = sendOk + 1;
+        }
+
+        if ($.trim(pass).length === 0) {
+            showError('La contraseña no puede ser vacía.');
+            $('input.password').focus();
+            return;
+        } else {
+            sendOk = sendOk + 1;
+        }
+
+        if (!validateEmail(username)) {
+            showError('El Email ingresado no es válido');
+            $('input.username').focus();
+            return;
+        } else {
+            sendOk = sendOk + 1;
+        }
+
+        if (sendOk == 3) {
+            //TODO: borrar datos fijos
+
+            /*
+             username = 'gpetti@aterrizar.com.ar';
+             pass = '30577262124';
+             */
+            $.ajax({
+                url: webServicesUrl + "login.php",
+                type: 'POST',
+                timeout: 5000,
+                data: {email: username, password: pass},
+                beforeSend: function () {
+                    $('.innerContainer *').not('h2').hide()
+                    $('.innerContainer p.loading').remove();
+                    $('.innerContainer').append('<p class="loading" style="margin-top:20px">Ingresando...</p>')
+                },
+                success: function (result) {
+                    var r = $.parseJSON(result);
+                    if (r.data && r.data.status && r.data.status == 'success') {
+                        var $lastlogin = '{"user": "' + $('input.username').val() + '", "pw": "' + $('input.password').val() + '"}';
+
+                        // Guardamos los datos necesarios del usuario                        
+                        localStorage.setItem('lastlogin', $lastlogin);
+                        localStorage.setItem("userID", r.data[0].userID);
+                        localStorage.setItem("lastUserID", r.data[0].userID);
+
+                        // Registramos el dispositivo
+                        
+
+                        // Redireccionamos a la home
+                        window.location.href = "home.html";
+                    } else {
+                        showError(r.data.message);
+                        $('.innerContainer *').show()
+                        $('.innerContainer p.loading').hide();
+                        $('input.username').focus();
+                    }
+                },
+                error: function (error) {
+                    showError('Error de conexión.');
+                    $('.innerContainer *').show()
+                    $('.innerContainer p.loading').hide();
+                    $('input.username').focus();
+                }
+            });
+        }
+        return;
+    });
+});
+
+
+
 function registerDevice() {
     try
     {
-        alert(device.platform);
-        alert(1);
         //pushNotification = window.plugins.pushNotification;
         if (device.platform == 'android' || device.platform == 'Android' ) {
             /* Registro si es android */
             window.plugins.pushNotification.register(successHandler, errorHandler, {"senderID":"888853500656","ecb":"onNotification"});
         } else {
-            alert(2);
             /* Registro si es ios */
             window.plugins.pushNotification.register(tokenHandler, errorHandler, {"badge":"true","sound":"true","alert":"true","ecb":"onNotificationAPN"});
-            
-            alert(3);
-            window.plugins.pushNotification.setApplicationIconBadgeNumber(successHandler, errorHandler, badgeCount);
-            var onDelay = function(){
-                alert('delay');
-                window.plugins.pushNotification.setApplicationIconBadgeNumber(0, function(){});
-            };
-            window.setTimeout(onDelay, 1000);
         }
+
+        window.plugins.pushNotification.setApplicationIconBadgeNumber(successHandler, errorHandler, badgeCount);
+        var onDelay = function(){
+            window.plugins.pushNotification.setApplicationIconBadgeNumber(0, function(){});
+        };
+        window.setTimeout(onDelay, 1000);
     }
     catch(err)
     {
-        showMessage('[registerDevice] ' + err.message);
-        //registerDeviceOk();
+        _alert(err.message)
     }
 }
 
-function successHandler(result) {
-    //alert('success');
-    //if (device.platform != 'android' && device.platform != 'Android' ) {
-        //badgeCount++;
-        //window.plugins.pushNotification.setApplicationIconBadgeNumber(successHandler, errorHandler, badgeCount);
-    //}
+function successHandler (result) {
+    badgeCount++;
+    window.plugins.pushNotification.setApplicationIconBadgeNumber(successHandler, errorHandler, badgeCount);
 }
 
 function errorHandler (error) {
-    showMessage('[errorHandler] Error del sistema: ' + error);
-
-    //registerDeviceOk();
+    showMessage('Error del sistema: ' + error);
 }
 
 function tokenHandler (result) {
-    alert(1);
-    try
-    {
-        // Your iOS push server needs to know the token before it can push to this device
-        // here is where you might want to send it the token for later use.
-        $.ajax({
-            url: webServicesUrl + 'registerDevice.php',
-            type:'POST',
-            data:{newDeviceID: result, newUserId: localStorage.getItem('userID'), platform: 'apns'},
-            success:function(result){
-                showMessage('[tokenHandler ok] ' + JSON.stringify(result));
-                var regID = result;
-                localStorage.setItem('phoneID',regID);
-
-                registerDeviceOk();
-            },
-            error:function(error){
-                showMessage('[tokenHandler error] ' + JSON.stringify(error));
-            },
-            complete:function(aa){
-                showMessage('[tokenHandler complete] ' + JSON.stringify(aa));
-            }
-        });
-    }
-    catch(err)
-    {
-        showMessage('[tokenHandler error catch] ' + err.message);
-        //registerDeviceOk();
-    }
+    // Your iOS push server needs to know the token before it can push to this device
+    // here is where you might want to send it the token for later use.
+    $.ajax({
+        url: webServicesUrl + 'registerDevice.php',
+        type:'POST',
+        data:{newDeviceID: result, newUserId: localStorage.getItem('userID'), platform: 'apns'},
+        success:function(result){
+            var regID = result;
+            localStorage.setItem('phoneID',regID);
+        },
+        error:function(error){
+            _alert(JSON.stringify(error));
+        }
+    });
 }
-
-
-
-// handle AjvPNS notifications for iOS
-function onNotificationAPN(e) {
-    alert(onNotificationAPN);
-    if (e.alert) {
-        // showing an alert also requires the org.apache.cordova.dialogs plugin
-        navigator.notification.confirm(
-            e.alert + '\n ¿Ver Ahora?', // message
-            onConfirm, // callback to invoke with index of button pressed
-            'Nuevo mensaje', // title
-            ['Si','Más Tarde'] // buttonLabels
-        );
-    }
-
-    if (e.sound) {
-        // playing a sound also requires the org.apache.cordova.media plugin
-        var snd = new Media(e.sound);
-        snd.play();
-    }
-
-    if (e.badge) {
-        window.plugins.pushNotification.setApplicationIconBadgeNumber(successHandler, errorHandler, e.badge);
-    }
-}
-
-
-// handle GCM notifications for Android
-function onNotification(e) {
-    switch( e.event )
-    {
-        case 'registered':
-            if ( e.regid.length > 0 )
-            {
-                //_alert("regID = " + e.regid);
-                $.ajax({
-                    url: webServicesUrl + 'registerDevice.php',
-                    type:'POST',
-                    data:{newDeviceID: e.regid, newUserId: localStorage.getItem('userID'), platform: 'gcm'},
-                    success:function(result){
-                        var regID = e.regid;
-                        localStorage.setItem('phoneID',regID);
-                        
-                        registerDeviceOk();
-                    },
-                    error:function(error){
-                        showMessage('[onNotification] ' + JSON.stringify(error));
-                    }
-                });
-            }
-            break;
-
-        case 'message':
-            if (e.foreground){
-                var mediaUrl = "/android_asset/www/sounds/knock.mp3";
-                var my_media = new Media(mediaUrl, function(){ my_media.release(); } , function(e){ _alert( JSON.stringify(e) ); });
-                my_media.play();
-            }
-
-            navigator.notification.confirm(
-                e.message + '\n ¿Ver Ahora?', // message
-                onConfirm, // callback to invoke with index of button pressed
-                'Nuevo mensaje', // title
-                ['Si','Más Tarde'] // buttonLabels
-            );
-
-            break;
-
-        case 'error':
-            showMessage('Ha ocurrido un error inesperado');
-            break;
-
-        default:
-            //showMessage('Unknown, an event was received and we do not know what it is');
-            break;
-    }
-}
-
-
 function unRegisterDevice(){
 
     $.ajax({
@@ -164,28 +171,16 @@ function unRegisterDevice(){
         success:function(result){
             //do something
             //console.log(result);
-            //showMessage("[unRegisterDevice OK]: " + JSON.stringify(result));
+            //localStorage.removeItem('phoneID');
         },
         error:function(error){
-            showMessage("[unRegisterDevice error]: " + JSON.stringify(error));
+            //showMessage("unRegisterDivace error: " + JSON.stringify(error));
         },
-        complete: function(result){
-            showMessage("[unRegisterDevice complete]" + JSON.stringify(result));
+        complete: function(){
             localStorage.removeItem('phoneID');
         }
     });
 }
 
 
-function onConfirm(buttonIndex) {
-    if(buttonIndex == 1){
-        window.location.href="contacts.html";
-    }
-}
 
-
-function registerDeviceOk()
-{
-    // Redireccionamos a la home
-    window.location.href = "home.html";   
-}
--- js/scriptHome.js
+++ js/scriptHome.js
@@ -1,5 +1,5 @@
 //var pushNotification = window.plugins.pushNotification;
-//var badgeCount = 0;
+var badgeCount = 0;
 $('#homePage').live( 'pageinit',function(event){
 
     setHeights();
@@ -13,11 +13,142 @@ $('#homePage').live( 'pageinit',function(event){
 });
 
 /* registrar push notifitions */
-//document.addEventListener('deviceready', registerDevice, true);
-//document.addEventListener('deviceready', notificationDevice, true);
+document.addEventListener('deviceready', registerDevice, true);
+
+function registerDevice() {
+    try
+    {
+        //pushNotification = window.plugins.pushNotification;
+        if (device.platform == 'android' || device.platform == 'Android' ) {
+            /* Registro si es android */
+            window.plugins.pushNotification.register(successHandler, errorHandler, {"senderID":"888853500656","ecb":"onNotification"});
+        } else {
+            /* Registro si es ios */
+            window.plugins.pushNotification.register(tokenHandler, errorHandler, {"badge":"true","sound":"true","alert":"true","ecb":"onNotificationAPN"});
+        }
+
+        window.plugins.pushNotification.setApplicationIconBadgeNumber(successHandler, errorHandler, badgeCount);
+        var onDelay = function(){
+            window.plugins.pushNotification.setApplicationIconBadgeNumber(0, function(){});
+        };
+        window.setTimeout(onDelay, 1000);
+    }
+    catch(err)
+    {
+        _alert(err.message)
+    }
+}
+
+// handle AjvPNS notifications for iOS
+function onNotificationAPN(e) {
+    if (e.alert) {
+        // showing an alert also requires the org.apache.cordova.dialogs plugin
+        navigator.notification.confirm(
+            e.alert + '\n ¿Ver Ahora?', // message
+            onConfirm, // callback to invoke with index of button pressed
+            'Nuevo mensaje', // title
+            ['Si','Más Tarde'] // buttonLabels
+        );
+    }
+
+    if (e.sound) {
+        // playing a sound also requires the org.apache.cordova.media plugin
+        var snd = new Media(e.sound);
+        snd.play();
+    }
+
+    if (e.badge) {
+        window.plugins.pushNotification.setApplicationIconBadgeNumber(successHandler, errorHandler, e.badge);
+    }
+}
+
+// handle GCM notifications for Android
+function onNotification(e) {
+    switch( e.event )
+    {
+        case 'registered':
+            if ( e.regid.length > 0 )
+            //save phone ID in localstorage
+            {
+                _alert("regID = " + e.regid);
+                $.ajax({
+                    url: webServicesUrl + 'registerDevice.php',
+                    type:'POST',
+                    data:{newDeviceID: e.regid, newUserId: localStorage.getItem('userID'), platform: 'gcm'},
+                    success:function(result){
+                        _alert(tokenHandler + ': ' +result)
+                        var regID = e.regid;
+                        localStorage.setItem('phoneID',regID)
+                    },
+                    error:function(error){
+                        _alert(JSON.stringify(error));
+                    }
+                });
+            }
+            break;
+
+        case 'message':
+            if (e.foreground){
+                var mediaUrl = "/android_asset/www/sounds/knock.mp3";
+                var my_media = new Media(mediaUrl, function(){ my_media.release(); } , function(e){ _alert( JSON.stringify(e) ); });
+                my_media.play();
+            }
+
+            navigator.notification.confirm(
+                e.message + '\n ¿Ver Ahora?', // message
+                onConfirm, // callback to invoke with index of button pressed
+                'Nuevo mensaje', // title
+                ['Si','Más Tarde'] // buttonLabels
+            );
+
+            break;
+
+        case 'error':
+            showMessage('Ha ocurrido un error inesperado');
+            break;
+
+        default:
+            //showMessage('Unknown, an event was received and we do not know what it is');
+            break;
+    }
+}
+
+function tokenHandler (result) {
+    // Your iOS push server needs to know the token before it can push to this device
+    // here is where you might want to send it the token for later use.
+    $.ajax({
+        url: webServicesUrl + 'registerDevice.php',
+        type:'POST',
+        data:{newDeviceID: result, newUserId: localStorage.getItem('userID'), platform: 'apns'},
+        success:function(result){
+            _alert(tokenHandler + ': ' +result)
+            var regID = result;
+            localStorage.setItem('phoneID',regID);
+        },
+        error:function(error){
+            _alert(JSON.stringify(error));
+        }
+    });
+}
 
 function _alert(msg){
     alert(msg);
+}
+
+
+function successHandler (result) {
+    badgeCount++;
+    window.plugins.pushNotification.setApplicationIconBadgeNumber(successHandler, errorHandler, badgeCount);
+}
+
+function errorHandler (error) {
+    //alert('Error del sistema: ' + error);
+}
+
+function onConfirm(buttonIndex) {
+    if(buttonIndex == 1){
+        window.location.href="contacts.html";
+    }
 }
 
 
